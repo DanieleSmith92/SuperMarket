c1958de2f32e7593bf0ae88a0082de9e88949e64
Lorenzo Leoni <Lorenzo Leoni@DESKTOP-70KN59K>
2018-06-18 17:12:35 +0200

    modifica service

--- src/main/java/it/dstech/SuperMarket/service/CartaCreditoService.java
+++ src/main/java/it/dstech/SuperMarket/service/CartaCreditoService.java
@@ -1,4 +1,4 @@
-package it.dstech.SuperMarket.security;
+package it.dstech.SuperMarket.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;

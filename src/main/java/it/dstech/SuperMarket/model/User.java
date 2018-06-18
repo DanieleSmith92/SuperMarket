d8db8ebe1d9dc0c916becf510f41ba877ecf19c3
vico <vicobb22@hotmail.com>
2018-06-18 16:19:01 +0200

    sec

--- src/main/java/it/dstech/SuperMarket/model/User.java
+++ src/main/java/it/dstech/SuperMarket/model/User.java
@@ -2,39 +2,27 @@ package it.dstech.SuperMarket.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 
 @Entity (name = "user")
 public class User extends Base {
 
+	@Column (name = "id", nullable = false, unique = true)
+	private String id;
 	@Column (name = "username", nullable = false, unique = true)
 	private String username;
 	@Column (name = "password", nullable = false, unique = true)
 	private String password;
-
+	
+	@Column (name = "via", nullable = false, unique = false)
+	private String via;
+	@Column (name = "cap", nullable = false, unique = true)
+	private String cap;
+	@Column (name = "citta", nullable = false, unique = true)
+	private String citta;
+	@Column (name = "provincia", nullable = false, unique = true)
+	private String prov;
+    @JoinColumn (name="profile_type")
 	private UserProfile profileType;
-
-
-	public UserProfile getProfileType() {
-		return profileType;
-	}
-
-	public void setProfileType(UserProfile profileType) {
-		this.profileType = profileType;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	
 }

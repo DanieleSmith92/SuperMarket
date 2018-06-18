73dc8d058f00a84f2a67e1f92e5927e031222283
cristian <cristian@cristian-HP-Laptop-15-bs0xx>
2018-06-18 16:46:11 +0200

    collegamento prodotto -> storico

--- src/main/java/it/dstech/SuperMarket/model/Storico.java
+++ src/main/java/it/dstech/SuperMarket/model/Storico.java
@@ -1,9 +1,15 @@
 package it.dstech.SuperMarket.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity(name = "storico")
 public class Storico extends Base{
@@ -14,4 +20,9 @@ public class Storico extends Base{
 	@Column(name = "data" , nullable = false)
 	private LocalDate data;
 
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_utente", nullable=false)
+	@JsonIgnore
+	private List<Prodotto>listaProdotti;
 }

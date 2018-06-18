ccd8c83785aed7a661267a06af4beb4305e84ba1
cristian <cristian@cristian-HP-Laptop-15-bs0xx>
2018-06-18 16:22:44 +0200

    ok

--- src/main/java/it/dstech/SuperMarket/model/Prodotto.java
+++ src/main/java/it/dstech/SuperMarket/model/Prodotto.java
@@ -15,11 +15,11 @@ public class Prodotto extends Base{
 	private String marca;
 	@Column(name="data_di_scadenza ", nullable = false , unique = false)
 	private LocalDate dataDiScadenza;
-	@JoinColumn(name="categoria ", nullable = false , unique = false)
+	@Column(name="categoria ", nullable = false , unique = false)
 	private Categoria categoria;
 	@Column(name="quantita_disponibile ", nullable = false , unique = false)
 	private int quantitaDisponibile;
-	@JoinColumn(name="unita ", nullable = false , unique = false)
+	@Column(name="unita ", nullable = false , unique = false)
 	private Unita unita;
 	@Column(name="prezzo_unitario ", nullable = false , unique = false)
 	private Double prezzoUnitario;

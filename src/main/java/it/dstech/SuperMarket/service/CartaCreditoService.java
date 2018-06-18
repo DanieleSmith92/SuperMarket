d624eee6c55cc9ce38de602d466d76525a92bd52
DanieleSmith <danielesmith92@gmail.com>
2018-06-18 17:39:39 +0200

    CartaCredito Service

--- src/main/java/it/dstech/SuperMarket/service/CartaCreditoService.java
+++ src/main/java/it/dstech/SuperMarket/service/CartaCreditoService.java
@@ -1,8 +1,12 @@
 package it.dstech.SuperMarket.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.dstech.SuperMarket.model.CartaCredito;
+import it.dstech.SuperMarket.model.User;
 import it.dstech.SuperMarket.repository.ICartaCreditoRepository;
 
 @Service
@@ -10,5 +14,24 @@ public class CartaCreditoService {
 
 	@Autowired
 	public ICartaCreditoRepository cartaCreditoRepository;
+	
+	@Autowired
+	private UserService userService;
+	
+	public CartaCredito findOne(Long id) {
+		return cartaCreditoRepository.findOne(id);
+	}
+	
+	public Iterable<CartaCredito> findAll() {
+		return cartaCreditoRepository.findAll();
+	}
+	
+	public CartaCredito create (Long idUser , CartaCredito carta) {
+		User user = userService.findOne(idUser);
+		List<CartaCredito> listaCarte = user.getListaCarte();
+		listaCarte.add(carta);
+		user.setListaCarte(listaCarte);
+		return cartaCreditoRepository.save(carta);
+	}
 	
 }

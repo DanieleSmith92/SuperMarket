b13b3a738acc281d794679dc06a304bda390a20a
Lorenzo Leoni <Lorenzo Leoni@DESKTOP-70KN59K>
2018-06-19 12:20:03 +0200

    CartaCreditoCrlDa finire

--- src/main/java/it/dstech/SuperMarket/api/CartaCreditoCtrl.java
+++ src/main/java/it/dstech/SuperMarket/api/CartaCreditoCtrl.java
@@ -1,10 +1,23 @@
 package it.dstech.SuperMarket.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.dstech.SuperMarket.model.CartaCredito;
+import it.dstech.SuperMarket.service.CartaCreditoService;
 
 @RestController
 @RequestMapping ("/cartaCredito")
 public class CartaCreditoCtrl {
 
+	@Autowired
+	CartaCreditoService serviceCartaCredito;
+	
+	public CartaCredito findOne(@PathVariable("id")Long id) {
+		return serviceCartaCredito.findOne(id);
+	}
+	
+	
 }

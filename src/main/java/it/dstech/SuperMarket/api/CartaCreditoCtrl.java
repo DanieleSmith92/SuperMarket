8eb377b674676ec488e06ebef892375247fb0abc
Lorenzo Leoni <Lorenzo Leoni@DESKTOP-70KN59K>
2018-06-19 15:22:39 +0200

    Controller Carta

--- src/main/java/it/dstech/SuperMarket/api/CartaCreditoCtrl.java
+++ src/main/java/it/dstech/SuperMarket/api/CartaCreditoCtrl.java
@@ -3,10 +3,13 @@ package it.dstech.SuperMarket.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.dstech.SuperMarket.model.CartaCredito;
 import it.dstech.SuperMarket.service.CartaCreditoService;
+
 
 @RestController
 @RequestMapping ("/cartaCredito")
@@ -14,10 +17,24 @@ public class CartaCreditoCtrl {
 
 	@Autowired
 	CartaCreditoService serviceCartaCredito;
-	
+
 	public CartaCredito findOne(@PathVariable("id")Long id) {
 		return serviceCartaCredito.findOne(id);
 	}
-	
-	
+	@RequestMapping(method=RequestMethod.GET, value="/findAll")
+	public Iterable<CartaCredito> findAll(){
+		return serviceCartaCredito.findAll();
+	}
+	@RequestMapping(method=RequestMethod.POST, value="/create")
+	public CartaCredito create(@RequestParam("carta") CartaCredito carta) {
+			return serviceCartaCredito.create(carta);
+	}
+	@RequestMapping(method=RequestMethod.POST, value="/update")
+	public CartaCredito update(@RequestParam("carta")CartaCredito carta) {
+		return serviceCartaCredito.update(carta);
+	}
+	@RequestMapping(method=RequestMethod.DELETE, value="/deleteOne")
+	public void deleteOne(Long idCarta) {
+		serviceCartaCredito.deleteOne(idCarta);
+	}
 }
--- src/main/java/it/dstech/SuperMarket/service/CartaCreditoService.java
+++ src/main/java/it/dstech/SuperMarket/service/CartaCreditoService.java
@@ -53,4 +53,5 @@ public class CartaCreditoService {
 		cartaCreditoRepository.delete(idCarta);
 	}
 	
+	
 }

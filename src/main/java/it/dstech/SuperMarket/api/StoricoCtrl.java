ae286fa30f79a306abdd9f58b63355a9879926cc
vico <vicobb22@hotmail.com>
2018-06-19 15:30:56 +0200

    storico

--- src/main/java/it/dstech/SuperMarket/api/StoricoCtrl.java
+++ src/main/java/it/dstech/SuperMarket/api/StoricoCtrl.java
@@ -1,10 +1,30 @@
 package it.dstech.SuperMarket.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RestController;
+
+
+import it.dstech.SuperMarket.model.Storico;
+
+import it.dstech.SuperMarket.service.StoricoService;
 
 @RestController
 @RequestMapping ("/storico")
 public class StoricoCtrl {
 
+	
+	@Autowired
+	private StoricoService storicoService;
+	@RequestMapping (method=RequestMethod.GET,value="/storicoPerUser")
+	public Storico storicoPerUser() {
+		return storicoService.storicoPerUser();
+	}
+	
+	
+	
+	
 }
